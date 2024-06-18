3e9b170bc0e1255fb6129ec2387650d9852bbc35
Eriking21 <64511659+Eriking21@users.noreply.github.com>
2024-06-18 19:45:20 +0100

    Atualizar o layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'SIG Consumo de Energia',
+  title: 'SIG Iluminação Pública',
   description: 'Software de monitoramento energético',
 }
 
